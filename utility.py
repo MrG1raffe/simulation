609a43d0f1f1058ae880e5a170b562fd292e28ac
Dimitri Sotnikov <dmitrysotni@gmail.com>
2024-11-11 16:27:21 +0100

    Initial commit

---
+++ utility.py
@@ -0,0 +1,22 @@
+import numpy as np
+from typing import Any
+from numpy.typing import NDArray
+from numpy import float_
+
+DEFAULT_SEED = 42
+
+def is_number(x: Any) -> bool:
+    """
+    Checks whether x is int or float.
+    """
+    return isinstance(x, int) or isinstance(x, float) or isinstance(x, complex)
+
+
+def to_numpy(x: Any) -> NDArray[float_]:
+    """
+    Converts x to numpy array.
+    """
+    if is_number(x):
+        return np.array([x])
+    else:
+        return np.array(x)
