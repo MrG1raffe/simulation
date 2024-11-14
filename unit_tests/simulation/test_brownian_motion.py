24687b6e6452006608482905954a9874edd67def
Воробьева Ирина <vib01@mail.ru>
2024-11-14 15:41:08 +0100

    Pull from main

--- unit_tests/simulation/test_brownian_motion.py
+++ unit_tests/simulation/test_brownian_motion.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from simulation.brownian_motion import simulate_brownian_motion_from_increments
+from simulation.brownian_motion import simulate_brownian_motion_from_increments, simulate_brownian_motion_from_brownian_bridge
 
 
 class TestSimulation(unittest.TestCase):
@@ -11,6 +11,20 @@ class TestSimulation(unittest.TestCase):
         size = 1_000
         dim = 3
         B = simulate_brownian_motion_from_increments(size=size, t_grid=t_grid, rng=rng, dim=dim)
+
+        self.assertTrue(B.shape == (size, dim, len(t_grid)))
+        self.assertTrue(np.allclose(np.mean(B, 0), 0, atol=1e-1))
+        self.assertTrue(np.allclose(np.var(B, 0), t_grid, rtol=1e-0))
+        self.assertTrue(np.isclose(np.var(np.diff(B, axis=2)), t_grid[1] - t_grid[0], rtol=1e-3))
+        self.assertTrue(np.isclose(np.mean(np.diff(B, axis=2)), 0, atol=1e-3))
+        self.assertTrue(np.allclose(np.corrcoef(np.diff(B, axis=2)[0]), np.eye(dim), atol=0.05))
+
+    def test_simulate_brownian_motion_from_brownian_bridge(self):
+        rng = np.random.default_rng(seed=42)
+        t_grid = np.linspace(0, 2, 1000)
+        size = 1_000
+        dim = 3
+        B = simulate_brownian_motion_from_brownian_bridge(size=size, t_grid=t_grid, rng=rng, dim=dim)
 
         self.assertTrue(B.shape == (size, dim, len(t_grid)))
         self.assertTrue(np.allclose(np.mean(B, 0), 0, atol=1e-1))
